1940310750851f60615cc4e010934c4e4fe60b1b
Ramyrashad127 <ramy41599@gmail.com>
2024-02-21 13:39:07 -0800

    new

--- models/city.py
+++ models/city.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel
+from models.base_model import Base
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    state_id = column(string(60), nullable=False, foreignkey('states.id'))
+    name = column(string(128), nullable=False)
