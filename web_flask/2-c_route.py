62e95fa9f97fad88f1fe8188fe14d2d06c4782cc
Ahmed El Gendy <elgendyahmed753@gmail.com>
2024-04-20 16:10:35 -0700

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Route /
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Route /hbnb
+    """
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Route /c/<text>
+    """
+    return "C " + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
